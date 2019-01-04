9be306a1dd96c4ed9b2e86ff2ca022b3cb76c745
Tiffany Simionescu <42684183+echoNeko87@users.noreply.github.com>
2019-01-04 13:27:34 -0500

    Changed NR_OF_SIMULTANEOUS_SOUNDS, includes High C

--- app/src/main/java/com/londonappbrewery/xylophonepm/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/xylophonepm/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
     private final int PRIORITY = 0;
     private final float NORMAL_PLAY_RATE = 1.0f;
 
-    // Member variables
+    // TODO: Add member variables here
     private SoundPool mSoundPool;  // empty member variable
     private int mCSoundId;
     private int mDSoundId;
@@ -32,11 +32,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // New SoundPool
+        // TODO: Create a new SoundPool
         final int NR_OF_SIMULTANEOUS_SOUNDS = 7;
         mSoundPool = new SoundPool(NR_OF_SIMULTANEOUS_SOUNDS, AudioManager.STREAM_MUSIC, 0);
 
-        // Load and get the IDs to identify the sounds
+        // TODO: Load and get the IDs to identify the sounds
         mCSoundId = mSoundPool.load(getApplicationContext(), R.raw.note1_c, 1);
         mDSoundId = mSoundPool.load(getApplicationContext(), R.raw.note2_d, 1);
         mESoundId = mSoundPool.load(getApplicationContext(), R.raw.note3_e, 1);
@@ -48,7 +48,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    // Play methods triggered by the buttons
+    // TODO: Add the play methods triggered by the buttons
     public void playC(View v) {
         Log.d("Xylophone", "Red Button Clicked!");
         mSoundPool.play(mCSoundId, LEFT_VOLUME, RIGHT_VOLUME, PRIORITY, NO_LOOP, NORMAL_PLAY_RATE);
